08a2fde0e8b1932a44e0a8625681104dc6dd3976
Alexander Zabluda <azabluda@gmail.com>
2024-03-19 15:52:59 +0100

    Back to 'Process _edm dropbox'
    Rationale: after dropping raw files into _edm folder the user might want to rename them prior to creation of .md's

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { Plugin, Notice, TAbstractFile, TFile, TFolder } from 'obsidian';
-import { v4 as uuidv4 } from 'uuid';
+import { Plugin, Notice, TFile, TFolder } from 'obsidian';
 import { format } from 'date-fns';
 
 export default class EdmPlugin extends Plugin {
@@ -9,31 +8,35 @@ export default class EdmPlugin extends Plugin {
 	}
 
 	onload() {
-		this.app.vault.on('create', async (fs: TAbstractFile) => {
-			await new Promise(resolve => setTimeout(resolve, 100));
-			const file = fs as TFile;
-			const drop = await this.forceFolder('_edm');
-			if (file && file.parent?.path == drop.path) {
+		this.addCommand({
+			id: 'process-edm-dropbox',
+			name: 'Process _edm dropbox',
+			callback: async () => {
+				const drop = await this.forceFolder('_edm');
+				const dropFiles = drop.children.filter(f => f instanceof TFile) as TFile[]
 				const sorted = await this.forceFolder('Sorted');
-				const ctime = format(new Date(file.stat.ctime), 'yyyy-MM-dd');
-				const mdBaseName = `${ctime} ${file.basename}`
-				const attDir = await this.forceFolder(`${sorted.path}/_attachments/${mdBaseName}`);
-				const mdPath = `${sorted.path}/${mdBaseName}.md`;
-				if (this.app.vault.getFileByPath(mdPath) != null) {
-					new Notice(`Note '${mdPath}' already exists 👎😕`);
-					return;
-				}
-				const mdData = `---
+				for (const file of dropFiles) {
+					const ctime = format(new Date(file.stat.ctime), 'yyyy-MM-dd');
+					const mdBaseName = `${ctime} ${file.basename}`
+					const attDir = await this.forceFolder(`${sorted.path}/_attachments/${mdBaseName}`);
+					const mdPath = `${sorted.path}/${mdBaseName}.md`;
+					if (this.app.vault.getFileByPath(mdPath) != null) {
+						new Notice(`Note '${mdPath}' already exists 👎😕`);
+						return;
+					}
+					const mdData = `---
 tags:
   - _UNSORTED_
 ---
 ![[${attDir.path}/${file.name}]]`
-				const msg = `Note '${mdPath}' created for '${file.name}' 🙂👍`;
-				await this.app.vault.create(mdPath, mdData)
-				await this.app.vault.rename(file, `${attDir.path}/${file.name}`);
-				new Notice(msg);
+					const msg = `Note '${mdPath}' created for '${file.name}' 🙂👍`;
+					await this.app.vault.create(mdPath, mdData)
+					await this.app.vault.rename(file, `${attDir.path}/${file.name}`);
+					new Notice(msg);
+	
+				}
 			}
-		})
+		});
 	}
 
 	onunload() {
