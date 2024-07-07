b0af20e201a642c2e42762fae1b3e9072b459e2d
Alexander Zabluda <azabluda@gmail.com>
2024-07-07 16:34:11 +0200

    Cosmetic

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { format } from 'date-fns';
 
 export default class EdmPlugin extends Plugin {
 
-	private async forceFolder(path: string) : Promise<TFolder> {
+	private async forceFolder(path: string): Promise<TFolder> {
 		return this.app.vault.getFolderByPath(path) ?? await this.app.vault.createFolder(path);
 	}
 
@@ -13,11 +13,11 @@ export default class EdmPlugin extends Plugin {
 			name: 'Process _edm dropbox',
 			callback: async () => {
 				const drop = await this.forceFolder('_edm');
-				const dropFiles = drop.children.filter(f => f instanceof TFile) as TFile[]
+				const dropFiles = drop.children.filter(f => f instanceof TFile) as TFile[];
 				const sorted = await this.forceFolder('Sorted');
 				for (const file of dropFiles) {
 					const ctime = format(new Date(file.stat.ctime), 'yyyy-MM-dd');
-					const mdBaseName = `${ctime} ${file.basename}`
+					const mdBaseName = `${ctime} ${file.basename}`;
 					const attDir = await this.forceFolder(`${sorted.path}/_attachments/${mdBaseName}`);
 					const mdPath = `${sorted.path}/${mdBaseName}.md`;
 					if (this.app.vault.getFileByPath(mdPath) != null) {
@@ -28,17 +28,15 @@ export default class EdmPlugin extends Plugin {
 tags:
   - _UNSORTED_
 ---
-![[${attDir.path}/${file.name}]]`
+![[${attDir.path}/${file.name}]]`;
 					const msg = `Note '${mdPath}' created for '${file.name}' 🙂👍`;
-					await this.app.vault.create(mdPath, mdData)
+					await this.app.vault.create(mdPath, mdData);
 					await this.app.vault.rename(file, `${attDir.path}/${file.name}`);
 					new Notice(msg);
-	
 				}
 			}
 		});
 	}
 
-	onunload() {
-	}
+	onunload() { }
 }
